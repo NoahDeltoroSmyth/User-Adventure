c3b73204d7e31ae18533aa8b23ac984df529f14f
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-26 22:52:14 -0700

    created button and half of eventlistener

--- Quest/quest.js
+++ Quest/quest.js
@@ -1,9 +1,10 @@
 import quests from '../Data/Quest-data.js';
-import { findById } from '../Utils.js';
+import { findById, getUser } from '../Utils.js';
 
 const params = new URLSearchParams(window.location.search);
 
 const data = findById(quests, params.get('id'));
+console.log(data, 'data');
 
 const title = document.getElementById('title');
 title.textContent = data.title;
@@ -23,6 +24,7 @@ for (let choice of data.choices) {
     input.name = 'choice';
     input.required = true;
     input.value = choice.id;
+    //console.log(choice.id);
 
     const span = document.createElement('span');
     span.textContent = choice.description;
@@ -30,3 +32,22 @@ for (let choice of data.choices) {
     label.append(input, span);
     choices.append(label);
 }
+
+const button = document.createElement('button');
+button.textContent = 'Choose';
+choices.append(button);
+
+choices.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const selectedRadio = document.querySelector('input[type="radio"]:checked');
+    console.log(selectedRadio, 'selectedradio');
+
+    const choice = findById(data.choices, selectedRadio.value);
+    console.log(choice, 'choice');
+
+    const user = getUser();
+    console.log(user, 'user');
+    
+
+});
